a5e896cfc7e6378dfc1dc984eaa0df78b5f4ee0c
Quwam Binuyo <qbinuyo7@gmail.com>
2025-04-17 13:14:55 +0100

    feat: enhance Error and HomeLayout components with improved structure and styling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link } from "react-router-dom";
+import img from "../assets/not-found.svg";
 
-const Error = () => {
-  return <h1>Error</h1>;
+const Error = ({ is404 = true }) => {
+  return (
+    <Wrapper>
+      <div>
+        {is404 ? (
+          <>
+            <img src={img} alt="not found" />
+            <h3>Ohh!</h3>
+            <p>We can't seem to find the page you're looking for.</p>
+          </>
+        ) : (
+          <>
+            <h3>Something went wrong</h3>
+            <p>Please try again later.</p>
+          </>
+        )}
+        <Link to="/">Back Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -7,7 +7,9 @@ const HomeLayout = () => {
     <>
       <div>
         <Navbar />
-        <Outlet />
+        <section className="page">
+          <Outlet />
+        </section>
       </div>
     </>
   );
